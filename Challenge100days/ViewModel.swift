0357a5617d0643cb0fad3c31fe28a11253adf4d7
koala panda <koakoa8823@protonmail.com>
2023-07-20 20:37:32 +0900

    当日のタスクが達成済みか判定する関数をViewModelに作成しました。

---
+++ Challenge100days/ViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  ViewModel.swift
+//  Challenge100days
+//
+//  Created by koala panda on 2023/07/20.
+//
+
+import SwiftUI
+
+struct ViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ViewModel_Previews: PreviewProvider {
+    static var previews: some View {
+        ViewModel()
+    }
+}
