1c292c93f6f779980d8bd8b0f3d41f9485b78b58
koala panda <koakoa8823@protonmail.com>
2023-07-20 21:25:35 +0900

    ViewModelの名前をNotificationViewModelに変更した

---
+++ Challenge100days/NotificationViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  NotificationViewModel.swift
+//  Challenge100days
+//
+//  Created by koala panda on 2023/07/20.
+//
+
+import SwiftUI
+
+struct NotificationViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NotificationViewModel_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationViewModel()
+    }
+}
