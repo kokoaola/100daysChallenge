5566279a864fec3b07c1747bb2f2026c65a9805e
koala panda <koakoa8823@protonmail.com>
2023-07-28 15:36:04 +0900

    新しくColorSaver.swiftファイルを追加

---
+++ Challenge100days/ColorSaver.swift
@@ -0,0 +1,58 @@
+//
+//  ColorSaveUserDefaults.swift
+//  Challenge100days
+//
+//  Created by koala panda on 2023/02/14.
+//
+
+import Foundation
+import SwiftUI
+import UIKit
+
+
+///引数で受け取った番号に応じた背景色を指定するViewModifier
+struct UserSettingGradient: ViewModifier{
+    let appColorNum: Int
+    var colors:[Color]{
+        switch appColorNum{
+        case 0:
+            return [.blue, .green]
+        case 1:
+            return [.green, .yellow]
+        case 2:
+            return [.purple, .blue]
+        case 3:
+            return [.black, .black]
+        default:
+            return [.blue, .green]
+        }
+    }
+    
+    func body(content: Content) -> some View{
+        content.background(.secondary).foregroundStyle(LinearGradient(
+            colors: [colors[0], colors[1]],
+            startPoint: .topLeading,
+            endPoint: .bottomTrailing
+        ))
+    }
+}
+
+/*
+ 使う時：
+ 
+ @State private var selectedColor = 0
+ VStack{
+     Picker(selection: $selectedColor) {
+         Text("青").tag(0)
+         Text("オレンジ").tag(1)
+         Text("紫").tag(2)
+         Text("モノトーン").tag(3)
+         } label: {
+         Text("アプリの色を変更する")
+     }
+ }
+ //背景グラデーション設定
+ .modifier(UserSettingGradient(appColorNum: selectedColor))
+ 
+ 
+ */
