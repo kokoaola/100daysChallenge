12c18c39e1929223b7efc8f2fefbc2100a93823b
koala panda <koakoa8823@protonmail.com>
2023-12-27 14:32:00 +0900

    ファイル階層の見直し

--- Challenge100days/Shared/Constants.swift
+++ Challenge100days/Shared/Constants.swift
@@ -29,3 +29,15 @@ struct AppGroupConstants{
     //CoreDataのentityName
     static let entityName = "DailyData"
 }
+
+
+struct UserDefaultsConstants{
+    ///ユーザーデフォルト用キー：目標用
+    static let longTermGoalKey = "longTermGoal"
+    ///ユーザーデフォルト用キー：取り組むこと用
+    static let shortTermGoalKey = "shortTermGoal"
+    ///ユーザーデフォルト用キー：初回起動確認用
+    static let isFirstKey = "isFirst"
+    ///ユーザーデフォルト用キー：アプリカラー選択用
+    static let userSelectedColorKey = "userSelectedColorKey"
+}
