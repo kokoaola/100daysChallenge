a02088cd65d2c9baae358748e96adf08612bc194
koala panda <koakoa8823@protonmail.com>
2023-12-27 16:13:36 +0900

    不要な関数の削除と、新しいメソッド・変数の追加

--- Challenge100days/Shared/GrobalStore.swift
+++ Challenge100days/Shared/GrobalStore.swift
@@ -1,72 +1,49 @@
 //
-//  Store.swift
-//  WeatherApp
+//  GrobalStore.swift
+//  Challenge100days
 //
-//  Created by koala panda on 2023/12/13.
+//  Created by koala panda on 2023/12/27.
 //
 
 import Foundation
-
+import CoreData
 
 ///目標と初回起動のフラグを格納するグローバルオブジェクト
-class Store: ObservableObject {
-    ///ユーザーデフォルト用の変数
-    private let defaults = UserDefaults.standard
-    
-    ///長期目標を格納する変数
-    @Published var longTermGoal:String{
-        didSet {
-            defaults.set(self.longTermGoal, forKey: UserDefaultsConstants.longTermGoalKey)
-        }
-    }
-    
-    ///短期目標を格納する変数
-    @Published var shortTermGoal:String{
-        didSet {
-            defaults.set(self.shortTermGoal, forKey: UserDefaultsConstants.shortTermGoalKey)
-        }
-    }
-    
-    ///初回起動確認用
-    @Published var isFirst: Bool{
-        didSet {
-            defaults.set(self.isFirst, forKey: UserDefaultsConstants.isFirstKey)
-        }
-    }
-    
-    ///背景色を格納する変数
-    @Published var userSelectedColor: Int
-    
+class GrobalStore: ObservableObject {
+    ///データコントローラー格納変数
+    let context = PersistenceController.persistentContainer.viewContext
+    ///データを全件格納する変数
+    @Published var allData : [DailyData] = []
+    ///今日が何日目のチャレンジか格納する変数
+    @Published var dayNumber: Int = 1
     ///全体で表示中のタブを格納する変数
     @Published var userSelectedTag = "one"
-    
-    ///アニメーション表示確認用
-    @Published var showAnimation: Bool = true
-
-    
-    
+    ///当日のタスクが達成済みかを格納する変数
+    var finishedTodaysTask: Bool{
+        guard let lastData = allData.last?.date else { return false }
+        return Calendar.current.isDate(Date.now, equalTo: lastData, toGranularity: .day)
+    }
     
     init(){
-        //初期値が入っていない場合は初回起動フラグにtrueを設定
-        defaults.register(defaults: ["isFirst":true])
-        //アプリ起動時はユーザーデフォルトからデータを取得
-        self.longTermGoal = defaults.string(forKey:UserDefaultsConstants.longTermGoalKey) ?? ""
-        self.shortTermGoal = defaults.string(forKey:UserDefaultsConstants.shortTermGoalKey) ?? ""
-        self.isFirst = defaults.bool(forKey:UserDefaultsConstants.isFirstKey)
-        self.userSelectedColor = defaults.integer(forKey:UserDefaultsConstants.userSelectedColorKey)
+        setAllData()
     }
     
-    ///ユーザーが選んだアプリの色を保存するメソッド
-    func saveUserSettingAppColor(){
-        defaults.set(self.userSelectedColor, forKey: UserDefaultsConstants.userSelectedColorKey)
-    }
     
-    ///ユーザーのカスタムセッティングをすべてリセットするメソッド
-    func resetUserSetting(){
-        userSelectedTag = "one"
-        self.longTermGoal = ""
-        self.shortTermGoal = ""
-        self.isFirst = true
-        self.userSelectedColor = 0
+    ///すべてのデータを取得するメソッド
+    func setAllData(){
+        let request = NSFetchRequest<DailyData>(entityName: AppGroupConstants.entityName)
+        request.sortDescriptors = [NSSortDescriptor(key: "date", ascending: true)]
+        do{
+            let tasks = try context.fetch(request)
+            allData = tasks
+        }catch{
+            allData = []
+        }
+        
+        if finishedTodaysTask{
+            dayNumber = allData.count
+        }else{
+            dayNumber = allData.count + 1
+        }
     }
 }
--- Challenge100days/Shared/ViewModelBase.swift
+++ Challenge100days/Shared/ViewModelBase.swift
@@ -12,30 +12,26 @@ class ViewModelBase: ObservableObject {
     ///ユーザーデフォルト用の変数
     let defaults = UserDefaults.standard
     
-    ///長期目標を格納する変数
-    @Published var longTermGoal:String{
-        didSet {
-            defaults.set(self.longTermGoal, forKey: UserDefaultsConstants.longTermGoalKey)
-        }
-    }
-    
-    ///短期目標を格納する変数
-    @Published var shortTermGoal:String{
-        didSet {
-            defaults.set(self.shortTermGoal, forKey: UserDefaultsConstants.shortTermGoalKey)
-        }
-    }
-    
+    ///背景色を格納する変数
+    @Published var userSelectedColor: Int
     ///初回起動確認用
     @Published var isFirst: Bool{
         didSet {
             defaults.set(self.isFirst, forKey: UserDefaultsConstants.isFirstKey)
         }
     }
-    
-    ///背景色を格納する変数
-    @Published var userSelectedColor: Int
-    
+    ///長期目標を格納する変数
+    @Published var longTermGoal: String{
+        didSet {
+            defaults.set(self.longTermGoal, forKey: UserDefaultsConstants.longTermGoalKey)
+        }
+    }
+    ///短期目標を格納する変数
+    @Published var shortTermGoal: String{
+        didSet {
+            defaults.set(self.shortTermGoal, forKey: UserDefaultsConstants.shortTermGoalKey)
+        }
+    }
     init(){
         //初期値が入っていない場合は初回起動フラグにtrueを設定
         defaults.register(defaults: ["isFirst":true])
