a1dd91607d6afdcf18474a7316e43d57a4dbcc30
koala panda <koakoa8823@protonmail.com>
2023-12-27 18:49:39 +0900

    ファイル名の変更と追加

---
+++ Challenge100days/AddMemoScreen/AddMemoViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  AddMemoViewModel.swift
+//  Challenge100days
+//
+//  Created by koala panda on 2023/12/27.
+//
+
+import Foundation
+
+
+final class AddMemoViewModel: ViewModelBase{
+    ///編集文章格納用
+    @Published var editText = ""
+}
