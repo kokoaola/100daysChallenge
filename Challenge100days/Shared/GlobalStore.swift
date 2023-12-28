7a430587abcb33f27e99a1343bb9f398bbf06fd3
koala panda <koakoa8823@protonmail.com>
2023-12-28 15:02:08 +0900

    起動時に日付などの表示が不安定になる問題を解決

--- Challenge100days/Shared/GlobalStore.swift
+++ Challenge100days/Shared/GlobalStore.swift
@@ -9,26 +9,30 @@ import Foundation
 import CoreData
 
 ///目標と初回起動のフラグを格納するグローバルオブジェクト
-class GrobalStore: ObservableObject {
+class GlobalStore: ObservableObject {
     ///データコントローラー格納変数
     let context = PersistenceController.persistentContainer.viewContext
     ///データを全件格納する変数
-    @Published var allData : [DailyData] = []
+    @Published var allData : [DailyData]
     ///今日が何日目のチャレンジか格納する変数
-    @Published var dayNumber: Int = 1
+    @Published var dayNumber: Int
     ///全体で表示中のタブを格納する変数
     @Published var userSelectedTag = "one"
     ///当日のタスクが達成済みかを格納する変数
-    @Published var finishedTodaysTask = false
+    @Published var finishedTodaysTask = true
+    let request = NSFetchRequest<DailyData>(entityName: AppGroupConstants.entityName)
     
     init(){
+        allData = []
+        dayNumber = 1
+        
         setAllData()
     }
     
     
     ///すべてのデータを取得するメソッド
     func setAllData(){
-        let request = NSFetchRequest<DailyData>(entityName: AppGroupConstants.entityName)
+        
         request.sortDescriptors = [NSSortDescriptor(key: "date", ascending: true)]
         do{
             let tasks = try context.fetch(request)
@@ -37,24 +41,49 @@ class GrobalStore: ObservableObject {
             }
         }catch{
             allData = []
+            dayNumber = 1
+            finishedTodaysTask = false
+            return
         }
         
         guard let lastData = allData.last?.date else {
+            self.objectWillChange.send()
             dayNumber = 1
             finishedTodaysTask = false
-            return }
+            return
+        }
         
         if Calendar.current.isDate(Date.now, equalTo: lastData, toGranularity: .day){
             DispatchQueue.main.async {
+                self.objectWillChange.send()
                 self.finishedTodaysTask = true
                 self.dayNumber = self.allData.count
             }
         }else{
             DispatchQueue.main.async {
+                self.objectWillChange.send()
                 self.finishedTodaysTask = false
                 self.dayNumber = self.allData.count + 1
             }
         }
     }
+    
+    ///データの番号を振り直すメソッド
+    func assignNumbers() async{
+        setAllData()
+        
+        await MainActor.run{
+            for (index, data) in self.allData.enumerated() {
+                // numを1から順に割り当てる
+                data.num = Int16(index + 1)
+            }
+            // 変更を保存
+            PersistenceController.shared.saveAsync { error in
+                if let _ = error {
+                } else {
+                    //                self.setAllData()
+                }
+            }
+        }
+    }
 }
-
